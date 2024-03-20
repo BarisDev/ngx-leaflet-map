465832c498bdae1916c3ff39ddb26347bdf7629d
BarisDev <baris-yarar@hotmail.com>
2024-03-21 00:01:48 +0300

    Environment updated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FeatureGroup, Marker, divIcon, icon, latLng, tileLayer } from 'leaflet';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -53,7 +54,7 @@ export class AppComponent implements OnInit {
         };
         tileLayers['Google Uydu'].addTo(this.map);
 
-        this.http.get('http://localhost:3000/points').subscribe((data: any) => {
+        this.http.get(environment.api + '/points').subscribe((data: any) => {
             data.forEach((el: any) => {
                 let byMarker = new Marker(latLng(el.lat, el.lng), {
                     draggable: false,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api: 'https://samm-api.onrender.com'
 };
